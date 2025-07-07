1804eb1fcd1d8574df86f53026c6337699b30069
Dave <dave@kinde.com>
2025-07-07 16:22:49 +1000

    Update Workflow.ts

--- kindeSrc/environment/workflows/planChange/Workflow.ts
+++ kindeSrc/environment/workflows/planChange/Workflow.ts
@@ -8,7 +8,7 @@ import {
 // The setting for this workflow
 export const workflowSettings: WorkflowSettings = {
   id: "onUserPlanSelection",
-  trigger: WorkflowTrigger.UserPlanSelection,
+  trigger: WorkflowTrigger.PlanSelection,
   name: "Plan change",
   failurePolicy: {
     action: "stop",
